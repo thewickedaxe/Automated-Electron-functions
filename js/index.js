44227503dfc8644d36592dc930063aba3525449b
Srinivas <srinivas.suresh.kumar@gmail.com>
2017-03-20 18:01:30 -0400

    Implemented clickable image buttons with hover text

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@ const url = require('url')
 
 var heading = document.getElementsByClassName("heading")[0]
 var intro_para = document.getElementsByClassName("intro_para")[0]
+var content = document.getElementsByClassName("actions")[0]
 heading.addEventListener("animationend", listener, false);
+intro_para.addEventListener("animationend", content_listener, false);
+content.style.display = "none"
 
 function listener(e) {
     console.log("Showing intro para")
     intro_para.style.visibility = 'visible'
+}
+function content_listener(e) {
+    console.log("Showing Content")
+    content.style.display = "table"
 }
